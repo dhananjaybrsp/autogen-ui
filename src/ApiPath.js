ee1759dd13e78a4f65a4e50a8a601c7a6b2ff48e
Kumar <dhananjay.kumar-p@brightspeed.com>
2024-02-02 22:08:44 +0530

    env file changes

--- src/ApiPath.js
+++ src/ApiPath.js
@@ -1,5 +1,4 @@
-// const ApiPath = import.meta.env.VITE_API_PATH
-
-const ApiPath = "ws://autogen-ui-6czzs5xdsq-uk.a.run.app/ws/";
-
+const ApiPath = import.meta.env.VITE_API_PATH
+// const ApiPath = "ws://autogen-ui-6czzs5xdsq-uk.a.run.app/ws/";
+// const ApiPath = "ws://localhost:8000/ws/";
 export default ApiPath;
